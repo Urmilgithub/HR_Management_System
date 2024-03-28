857424c1291f6e3f18195ab575feb37d62c4cf38
Urmilgithub <urmilpatelit27@gmail.com>
2024-03-28 23:31:15 +0530

    feat: created dbcontext file

---
+++ HR_Management_API/DbContext/Data_DBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace hr_management_api.dbcontext
+{
+    public class data_dbcontext : DbContext
+    {
+        public data_dbcontext(DbContextOptions dbcontextoptions) : base(dbcontextoptions)
+        {
+        }
+
+    }
+}
