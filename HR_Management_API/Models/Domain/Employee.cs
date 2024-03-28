ad1198d8c87cc2c78b2ea9ea8074b2436eb306b8
Urmilgithub <urmilpatelit27@gmail.com>
2024-03-28 23:34:41 +0530

    feat: Add employee domain model

---
+++ HR_Management_API/Models/Domain/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HR_Management_API.Models.Domain
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int Contact { get; set; }
+        public DateTime HireDate { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
