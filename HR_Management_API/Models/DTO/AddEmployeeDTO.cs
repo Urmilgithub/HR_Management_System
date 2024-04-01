c47c1f436937cbcd0d62811e0382b2f3550bf471
Urmilgithub <urmilpatelit27@gmail.com>
2024-04-01 22:23:55 +0530

    feat: Add employee add dto

---
+++ HR_Management_API/Models/DTO/AddEmployeeDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace HR_Management_API.Models.DTO
+{
+    public class AddEmployeeDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int Contact { get; set; }
+        public DateTime HireDate { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
