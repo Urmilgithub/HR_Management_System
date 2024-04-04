918c0453e031a1672e1bce3cda6b92f151f750cf
Urmilgithub <urmilpatelit27@gmail.com>
2024-04-04 22:48:25 +0530

    feat: Add job domain model

---
+++ HR_Management_API/Models/Domain/Job.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HR_Management_API.Models.Domain
+{
+    public class Job
+    {
+        [Key]
+        public int JobId { get; set; }
+        public string JobTitle { get; set; }
+        public decimal MinSalary { get; set; }
+        public decimal MaxSalary { get; set; }
+    }
+}
