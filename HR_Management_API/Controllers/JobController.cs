8892a81ee7f9b8335157d8c95fcc94cae5427779
Urmilgithub <urmilpatelit27@gmail.com>
2024-04-05 22:02:24 +0530

    feat: Add job get all method

---
+++ HR_Management_API/Controllers/JobController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using HR_Management_API.Models.DTO;
+using HR_Management_API.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HR_Management_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobController : ControllerBase
+    {
+        private readonly IJobRepository jobRepository;
+        private readonly IMapper mapper;
+
+        public JobController(IJobRepository _jobRepository, IMapper _mapper)
+        {
+            jobRepository = _jobRepository;
+            mapper = _mapper;
+        }
+
+        [HttpGet("GetAllJobs")]
+        public async Task<IActionResult> GetAllJobs()
+        {
+            var jobDomain = await jobRepository.GetAllJobs();
+            return Ok(mapper.Map<JobDTO>(jobDomain));
+        }
+    }
+}
