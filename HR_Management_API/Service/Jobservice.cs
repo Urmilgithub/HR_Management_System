ebc0cef53d1edaf4f168e91df3ec78c1ea45abff
Urmilgithub <urmilpatelit27@gmail.com>
2024-04-05 21:55:49 +0530

    feat: Add job get all service

---
+++ HR_Management_API/Service/Jobservice.cs
@@ -0,0 +1,20 @@
+﻿using HR_Management_API.DBContext;
+using HR_Management_API.Models.Domain;
+using HR_Management_API.Repository;
+
+namespace HR_Management_API.Service
+{
+    public class Jobservice : IJobRepository
+    {
+        private readonly Data_DBContext dBContext;
+
+        public Jobservice(Data_DBContext _dBContext)
+        {
+            dBContext = _dBContext;
+        }
+        public async Task<List<Job>> GetAllJobs()
+        {
+            return await dBContext.Jobs.ToListAsync();
+        }
+    }
+}
