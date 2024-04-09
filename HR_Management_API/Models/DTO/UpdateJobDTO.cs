122b98d5018c1fd3c17ffe24116db4610800108b
Urmilgithub <urmilpatelit27@gmail.com>
2024-04-09 23:27:32 +0530

    feat: Add update job dto

---
+++ HR_Management_API/Models/DTO/UpdateJobDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace HR_Management_API.Models.DTO
+{
+    public class UpdateJobDTO
+    {
+        public string JobTitle { get; set; }
+        public decimal MinSalary { get; set; }
+        public decimal MaxSalary { get; set; }
+    }
+}
