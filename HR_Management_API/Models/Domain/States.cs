016db15e000c81b6892d2b842590f912a3a6e908
Urmilgithub <urmilpatelit27@gmail.com>
2024-04-10 22:48:29 +0530

    feat: Add state domain model

---
+++ HR_Management_API/Models/Domain/States.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HR_Management_API.Models.Domain
+{
+    public class States
+    {
+        [Key]
+        public int StateId { get; set; }
+        public string StateName { get; set; }
+    }
+}
