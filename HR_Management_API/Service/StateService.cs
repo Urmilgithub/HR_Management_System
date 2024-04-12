9ccc060f136d681e7dafd7eb4caa7a1a85f04905
Urmilgithub <urmilpatelit27@gmail.com>
2024-04-12 22:58:38 +0530

    feat: Add get all states service

--- HR_Management_API/Service/StateService.cs
+++ HR_Management_API/Service/StateService.cs
@@ -1,8 +1,23 @@
-﻿using HR_Management_API.Repository;
+﻿using HR_Management_API.DBContext;
+using HR_Management_API.Models.Domain;
+using HR_Management_API.Repository;
+using Microsoft.EntityFrameworkCore;
 
 namespace HR_Management_API.Service
 {
-    public class StateService: IStateRepository
+    public class StateService : IStateRepository
     {
+        private readonly Data_DBContext dBContext;
+
+        public StateService(Data_DBContext _dBContext)
+        {
+            dBContext = _dBContext;
+        }
+
+
+        public async Task<List<State>> GetAllStates()
+        {
+            return await dBContext.States.ToListAsync();
+        }
     }
 }
