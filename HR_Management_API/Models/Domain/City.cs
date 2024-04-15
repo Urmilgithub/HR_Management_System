e5f39d22503a042e9c0e9fb6a31e9a80d70be6a5
Urmilgithub <urmilpatelit27@gmail.com>
2024-04-15 23:18:07 +0530

    feat: Add city domain model

---
+++ HR_Management_API/Models/Domain/City.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HR_Management_API.Models.Domain
+{
+    public class City
+    {
+        [Key]
+        public int CityId { get; set; }
+        public string CityName { get; set; }
+
+        public int StateId { get; set; }
+    }
+}
