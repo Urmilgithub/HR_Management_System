30984d6095f2557687653b60234859e9afc7e061
Urmilgithub <urmilpatelit27@gmail.com>
2024-04-16 23:35:17 +0530

    feat: Add city list service

---
+++ HR_Management_API/Service/CityService.cs
@@ -0,0 +1,22 @@
+﻿using HR_Management_API.DBContext;
+using HR_Management_API.Models.Domain;
+using HR_Management_API.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace HR_Management_API.Service
+{
+    public class CityService : ICityRepository
+    {
+        private readonly Data_DBContext dBContext;
+
+        public CityService(Data_DBContext _dBContext)
+        {
+            dBContext = _dBContext;
+        }
+
+        public async Task<List<City>> GetCityListAsync()
+        {
+            return await dBContext.Cities.ToListAsync();
+        }
+    }
+}
