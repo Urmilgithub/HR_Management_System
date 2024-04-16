727e3070c0780a9ac7dba896ebc0e60dc1785fc5
Urmilgithub <urmilpatelit27@gmail.com>
2024-04-16 23:42:42 +0530

    feat: Add city get all method

---
+++ HR_Management_API/Controllers/CityController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using HR_Management_API.Models.DTO;
+using HR_Management_API.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HR_Management_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        private readonly ICityRepository cityRepository;
+        private readonly IMapper mapper;
+
+        public CityController(ICityRepository _cityRepository, IMapper _mapper)
+        {
+            cityRepository = _cityRepository;
+            mapper = _mapper;
+        }
+
+        [HttpGet("GetCityList")]
+        public async Task<IActionResult> GetCity()
+        {
+            var cityDomain = await cityRepository.GetCityListAsync();
+            return Ok(mapper.Map<CityDTO>(cityDomain));
+        }
+    }
+}
