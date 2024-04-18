b4094e436ee9ebdeedf53de87ca87e0a5155a69d
Urmilgithub <urmilpatelit27@gmail.com>
2024-04-18 23:02:30 +0530

    feat: Add department domain model

---
+++ HR_Management_API/Models/Domain/Department.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HR_Management_API.Models.Domain
+{
+    public class Department
+    {
+        [Key]
+        public int DeptId { get; set; }
+        public string DeptName { get; set; }
+        public int CityId { get; set; }
+    }
+}
