2e97bbf61128b18464a4a560ca18fcd515a4a3ea
Urmilgithub <urmilpatelit27@gmail.com>
2024-04-18 23:08:34 +0530

    feat: Add department update dto

---
+++ HR_Management_API/Models/DTO/UpdateDepartmentDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace HR_Management_API.Models.DTO
+{
+    public class UpdateDepartmentDTO
+    {
+        public string DeptName { get; set; }
+        public int CityId { get; set; }
+    }
+}
