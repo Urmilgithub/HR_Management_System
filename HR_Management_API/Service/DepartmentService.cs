d2d3bc6b54f498d2a720f424bd48e5d82047d21f
Urmilgithub <urmilpatelit27@gmail.com>
2024-04-18 23:41:06 +0530

    feat: Add get all dept service

--- HR_Management_API/Service/DepartmentService.cs
+++ HR_Management_API/Service/DepartmentService.cs
@@ -1,6 +1,22 @@
-﻿namespace HR_Management_API.Service
+﻿using HR_Management_API.DBContext;
+using HR_Management_API.Models.Domain;
+using HR_Management_API.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace HR_Management_API.Service
 {
-    public class DepartmentService
+    public class DepartmentService : IDepartmentRepository
     {
+        private readonly Data_DBContext dBContext;
+
+        public DepartmentService(Data_DBContext _dBContext)
+        {
+            dBContext = _dBContext;
+        }
+
+        public async Task<List<Department>> GetDepartmentListAsync()
+        {
+            return await dBContext.Departments.ToListAsync();
+        }
     }
 }
